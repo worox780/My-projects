c4f6a43cf5671b51db54ab85c989b7ffbf5768a2
worox780 <spikala06@gmail.com>
2023-05-04 20:05:27 +0200

    New page index in contact
    For orgranisation and creation, we are creating the index page on the contact page.

--- js/contact.js
+++ js/contact.js
@@ -1,14 +1,44 @@
 window.addEventListener('scroll', function() {
-    if ((window.scrollY + window.innerHeight > document.getElementById('Big-title-intro').offsetTop)) {
-        document.getElementById('Big-title-intro').classList.add("show")
+    if (window.scrollY + window.innerHeight > document.getElementById('Big-title-intro').offsetTop && window.scrollY >= 1) {
+        document.getElementById('Big-title-intro').classList.add("show");
     }
     else {
-        document.getElementById('Big-title-intro').classList.remove("show")
+        document.getElementById('Big-title-intro').classList.remove("show");
     }
-    if ((window.scrollY + window.innerHeight > document.getElementById('Medium-title-part1').offsetTop)) {
-        document.getElementById('Medium-title-part1').classList.add("show")
+    if (window.scrollY + window.innerHeight > document.getElementById('container-intro').offsetTop) {
+        
+        if (window.scrollY + window.innerHeight > document.getElementById('img-intro-contra').offsetTop && window.scrollY >= 200) {
+            document.getElementById('img-intro-contra').classList.add("show")
+        }else {
+            document.getElementById('img-intro-contra').classList.remove("show")
+        }
+
+        if (window.scrollY + window.innerHeight > document.getElementById('container-texte-intro').offsetTop && window.scrollY >= 200) {
+            document.getElementById('container-texte-intro').classList.add("show")
+        }else {
+            document.getElementById('container-texte-intro').classList.remove("show")
+        }
+        
+        if (window.scrollY + window.innerHeight > document.getElementById('Medium-title-part1').offsetTop && window.scrollY >= 200) {
+            document.getElementById('Medium-title-part1').classList.add("show")
+        }else {
+            document.getElementById('Medium-title-part1').classList.remove("show")
+        }
+        if (window.scrollY + window.innerHeight > document.getElementById('txt-intro-def').offsetTop + 200 && window.scrollY + window.innerHeight < document.getElementById('txt-intro-def').offsetTop + 350) {
+            document.getElementById('ph1-intro').classList.add('higlite')
+        }else {
+            document.getElementById('ph1-intro').classList.remove('higlite')
+        }
+        if (window.scrollY + window.innerHeight > document.getElementById('txt-intro-def').offsetTop + 350 && window.scrollY + window.innerHeight < document.getElementById('txt-intro-def').offsetTop + 500) {
+            document.getElementById('ph2-intro').classList.add('higlite')
+        }else {
+            document.getElementById('ph2-intro').classList.remove('higlite')
+        }
+        if (window.scrollY + window.innerHeight > document.getElementById('txt-intro-def').offsetTop + 500) {
+            document.getElementById('ph3-intro').classList.add('higlite')
+        }else {
+            document.getElementById('ph3-intro').classList.remove('higlite')
+        }
     }
-    else {
-        document.getElementById('Medium-title-part1').classList.remove("show")
-    }
+    
 })
