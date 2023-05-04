bd1528d7e134e7dfb7a14a8f80498f4856ce0fa9
worox780 <spikala06@gmail.com>
2023-05-05 00:37:55 +0200

    New index page in contacte page

--- js/contact.js
+++ js/contact.js
@@ -40,5 +40,52 @@ window.addEventListener('scroll', function() {
             document.getElementById('ph3-intro').classList.remove('higlite')
         }
     }
-    
+    if (window.scrollY + window.innerHeight > document.getElementById('Big-title-part2').offsetTop) {
+        document.getElementById('Big-title-part2').classList.add("show");
+    }
+    else {
+        document.getElementById('Big-title-part2').classList.remove("show");
+    }
+    if (window.scrollY + window.innerHeight > document.getElementById('Big-title-part3').offsetTop) {
+        document.getElementById('Big-title-part3').classList.add("show");
+    }
+    else {
+        document.getElementById('Big-title-part3').classList.remove("show");
+    }
 })
+
+
+window.addEventListener('scroll', function() {
+    
+    if (window.scrollY + window.innerHeight > document.getElementById('frise-container1').offsetTop) {
+        document.getElementById('frise-container1').classList.add("moov");
+    }
+    else {
+        document.getElementById('frise-container1').classList.remove("moov");
+    }
+    if (window.scrollY + window.innerHeight > document.getElementById('frise-container2').offsetTop) {
+        document.getElementById('frise-container2').classList.add("moov");
+    }
+    else {
+        document.getElementById('frise-container2').classList.remove("moov");
+    }
+    if (window.scrollY + window.innerHeight > document.getElementById('frise-container3').offsetTop) {
+        document.getElementById('frise-container3').classList.add("moov");
+    }
+    else {
+        document.getElementById('frise-container3').classList.remove("moov");
+    }
+    if (window.scrollY + window.innerHeight > document.getElementById('frise-container4').offsetTop) {
+        document.getElementById('frise-container4').classList.add("moov");
+    }
+    else {
+        document.getElementById('frise-container4').classList.remove("moov");
+    }
+    if (window.scrollY + window.innerHeight > document.getElementById('frise-container5').offsetTop) {
+        document.getElementById('frise-container5').classList.add("moov");
+    }
+    else {
+        document.getElementById('frise-container5').classList.remove("moov");
+    }
+  });
+  
