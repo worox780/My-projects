3255c94fd5f7beb7b4dcb02425f158d0ef33a2b0
worox780 <spikala06@gmail.com>
2023-05-08 20:33:45 +0200

    New

--- js/contraception-urgence.js
+++ js/contraception-urgence.js
@@ -62,6 +62,13 @@ window.addEventListener('scroll', function() {
         }
 });
 
+const counter1 = document.getElementById('pourcentage1-prise0');
+let can_pourcentage_1 = true;
+const counter2 = document.getElementById('pourcentage1-prise24');
+const counter3 = document.getElementById('pourcentage1-prise48');
+const counter4 = document.getElementById('pourcentage2-prise0');
+let can_pourcentage_2 = true;
+const counter5 = document.getElementById('pourcentage2-prise72');
 window.addEventListener('scroll', function() {
     if (window.scrollY + window.innerHeight > document.getElementById('Big-title-part2').offsetTop) {
         document.getElementById('Big-title-part2').classList.add("show");
@@ -111,5 +118,83 @@ window.addEventListener('scroll', function() {
     }
     else {
         document.getElementById('paragraphe2-part2-2').classList.remove("show");
+    }
+
+    if (window.scrollY + window.innerHeight > document.getElementById('chiffres-clés').offsetTop + 300 && can_pourcentage_1) {
+        can_pourcentage_1 = false;
+        let count1 = 0;
+        let count2 = 0;
+        let count3 = 0;
+        const duration = 1000; // en millisecondes
+        const interval = 10; // en millisecondes
+
+        function increment1() {
+        count1 += 99.6 / (duration / interval);
+        if (count1 > 99.6) {
+            count1 = 99.6;
+        }
+        counter1.innerHTML = count1.toFixed(1);
+        if (count1 < 99.6) {
+            setTimeout(increment1, interval);
+        }
+        }
+        setTimeout(increment1, interval);
+
+        function increment2() {
+            count2 += 98.8 / (duration / interval);
+            if (count2 > 98.8) {
+                count2 = 98.8;
+            }
+            counter2.innerHTML = count2.toFixed(1);
+            if (count2 < 98.8) {
+                setTimeout(increment2, interval);
+            }
+            }
+        setTimeout(increment2, interval);
+
+        function increment3() {
+            count3 += 97.3 / (duration / interval);
+            if (count3 > 97.3) {
+                count3 = 97.3;
+            }
+            counter3.innerHTML = count3.toFixed(1);
+            if (count3 < 97.3) {
+                setTimeout(increment3, interval);
+            }
+        }
+        setTimeout(increment3, interval);
+    }
+
+    if (window.scrollY + window.innerHeight > document.getElementById('chiffres-clés2').offsetTop + 300 && can_pourcentage_2) {
+        can_pourcentage_2 = false;
+        let count1 = 0;
+        let count2 = 0;
+        let count3 = 0;
+        const duration = 1000; // en millisecondes
+        const interval = 10; // en millisecondes
+
+        function increment1() {
+        count1 += 98.5 / (duration / interval);
+        if (count1 > 98.5) {
+            count1 = 98.5;
+        }
+        counter4.innerHTML = count1.toFixed(1);
+        if (count1 < 98.5) {
+            setTimeout(increment1, interval);
+        }
+        }
+        setTimeout(increment1, interval);
+
+        function increment2() {
+            count2 += 98.3 / (duration / interval);
+            if (count2 > 98.3) {
+                count2 = 98.3;
+            }
+            counter5.innerHTML = count2.toFixed(1);
+            if (count2 < 98.3) {
+                setTimeout(increment2, interval);
+            }
+            }
+        setTimeout(increment2, interval);
     }
 })
