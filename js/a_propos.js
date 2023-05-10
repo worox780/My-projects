c2fad6fcdb0711843d4798aacacfcaf6e9909061
worox780 <spikala06@gmail.com>
2023-05-10 23:51:33 +0200

    New

--- js/a_propos.js
+++ js/a_propos.js
@@ -1,34 +1,4 @@
+this.document.getElementById('part4').style.top = window.screen.height
 window.addEventListener('scroll', function() {
-    
-    if (window.scrollY + window.innerHeight > document.getElementById('frise-container1').offsetTop) {
-        document.getElementById('frise-container1').classList.add("moov");
-    }
-    else {
-        document.getElementById('frise-container1').classList.remove("moov");
-    }
-    if (window.scrollY + window.innerHeight > document.getElementById('frise-container2').offsetTop) {
-        document.getElementById('frise-container2').classList.add("moov");
-    }
-    else {
-        document.getElementById('frise-container2').classList.remove("moov");
-    }
-    if (window.scrollY + window.innerHeight > document.getElementById('frise-container3').offsetTop) {
-        document.getElementById('frise-container3').classList.add("moov");
-    }
-    else {
-        document.getElementById('frise-container3').classList.remove("moov");
-    }
-    if (window.scrollY + window.innerHeight > document.getElementById('frise-container4').offsetTop) {
-        document.getElementById('frise-container4').classList.add("moov");
-    }
-    else {
-        document.getElementById('frise-container4').classList.remove("moov");
-    }
-    if (window.scrollY + window.innerHeight > document.getElementById('frise-container5').offsetTop) {
-        document.getElementById('frise-container5').classList.add("moov");
-    }
-    else {
-        document.getElementById('frise-container5').classList.remove("moov");
-    }
   });
   
